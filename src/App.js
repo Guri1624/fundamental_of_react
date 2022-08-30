8a1450f0edf3a057c90b2b2ef641a8472ef030de
guri <gurmeetguri16124@gmail.com>
2022-08-30 12:52:12 +0530

    boot

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 
 import logo from './logo.svg';
-import React, { useEffect, useState } from 'react'
-import Uefeect from './Uefeect'
-function App(){
-     const [data,setData]=useState(10);
-     const [count,setCount]=useState(10);
-     
+import './App.css'
+import React from 'react'
+import {Button} from 'react-bootstrap'
+function App(){ 
      return(
      <div className="App">
-          <Uefeect count={count} data={data}/>
-        <button onClick={()=>setData(data+1)}>update data </button>
-         <button onClick={()=>setCount(count+1)}>update count </button>
+       <h1 > intall bootsrap</h1>
+       <Button onClick={()=>alert("hello")}>click me</Button>
+       <button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+<button type="button" class="btn btn-link">link</button>
         </div>
      );
 }
